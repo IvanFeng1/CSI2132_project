3f191f29b4dc79a27cbf7e8f14eb7f83902c26b7
Ahmed A <56705365+qaiys@users.noreply.github.com>
2021-03-30 16:21:26 -0400

    Update wee.py

--- wee.py
+++ wee.py
@@ -1,13 +1,15 @@
 import psycopg2
-
+f = open("cred.txt","r")
+uname = f.readline()
+upass = f.readline()
+uname = uname[0:len(uname)-1]
+upass = upass[0:len(upass)-1]
 # connect to db
 con = psycopg2.connect(
     host = "web0.eecs.uottawa.ca",
     database = "group_a03_g04",
-    user="", # uottawa email without @uottawa.ca
-    # remove next line
-    password ="",# remove this REMMBER!!!!!!!!!  password of uottawa email REMEMBER TO REMOVE WHEN PUSHING
-    # remove remove remove 
+    user=uname, 
+    password =upass,
     port="15432"
 )
 
