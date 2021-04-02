97e85dec7ecb3d09518b38a3d2202dc7c907a63b
Ahmed A <56705365+qaiys@users.noreply.github.com>
2021-04-02 05:58:45 -0400

    Update wee.py

--- wee.py
+++ wee.py
@@ -15,12 +15,23 @@ con = psycopg2.connect(
 
 cursor = con.cursor()
 
+def printQuery(z, n=10):
+    if len(z) < n:
+        n = len(z)
+    print("+-----------+")
+    for i in range(n):
+        x = z[i]
+        for y in x: 
+            print("+ "+str(y))
+        print("+-----------+")
+    
+
 
 
 def title(): #main menu. admin and employee should prolly get prompted for passwords n stuff
     title= "+------------------------------------+\n|                                    |\n|                                    |\n|                                    |\n|                                    |\n| [A]: Customer                      |\n| [B]: Employee                      |\n| [C]: Admin                         |\n|                                    |\n|                                    |\n+------------------------------------+"
     print(title)
-    x = input()
+    x = input("[ ]:")
     if x == "A":
         customer()
     elif x == "B":
@@ -29,21 +40,39 @@ def title(): #main menu. admin and employee should prolly get prompted for passw
         admin()
 
 def cSearch(): #this will later take as input the date n stuff. db as is now is empty so idk what to do with them
+        print("[1]: Daisu")
+        print("[2]: Ivay")
+        print("[3]: Shulizany")
+        print("[4]: Lame Places")
+        print("[5]: Lotsa Rooms")
+        print("[6]: Old Zealand")
+        print("[7]: Any")
+        print("Which Hotel Company?:")
+        x1 = input("[ ]:")
+        if x1 == "7":
+            x1 = "*"
+        x2 = input("Before/Exactly/After (B/E/A): \n")
+        x3 = input("Date: (YY-MM-DD)\n")
+        cursor.execute("SELECT room_num FROM \"Project\".room") #join room to 
         print("| Avaliable Rooms by Number:         |")
-        cursor.execute("SELECT room_num FROM \"Project\".Room")
-        print(cursor.fetchall())
+        printQuery(cursor.fetchall())
 
 def cBook():
-    n = input("Enter Room Number: ")
+    n = input("Enter Room Number: \n")
+    br = input("[1]: Booking\n[2]: Renting\nBooking or Renting?:\n")
+    chk = input("When will you check in?: (YY-MM-DD)\n")
+    occ = input("How many occupants?: \n")
+
     # *do some stuff*
 
 def customer():
+    print("+--------------------------+")
     print("1: Search Avaliable Rooms")
     print("2: Book a Room")
-    x = input()
+    print("+--------------------------+")
+    x = input("[ ]:")
     if x == "1":
-        a = input("Before/Exactly/After (B/E/A): ")
-        b = input("Date: (YY-MM-DD)")
+
         cSearch() #a and b take as args 
     elif x == "2":
         cBook()
