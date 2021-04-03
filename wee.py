ff528527dbd48026cd45835556786bbe79b384da
Ahmed A <56705365+qaiys@users.noreply.github.com>
2021-04-03 06:31:24 -0400

    Update wee.py
    employee search and rentings

--- wee.py
+++ wee.py
@@ -8,16 +8,6 @@ con = psycopg2.connect(
     host = "web0.eecs.uottawa.ca",
     database = "group_a03_g04",
     user=uname, 
-    password =upass,import psycopg2
-f = open("cred.txt","r")
-uname = f.readline()
-upass = f.readline()
-uname = uname[0:len(uname)-1]
-# connect to db
-con = psycopg2.connect(
-    host = "web0.eecs.uottawa.ca",
-    database = "group_a03_g04",
-    user=uname, 
     password =upass,
     port="15432"
 )
@@ -53,24 +43,40 @@ def cSearch(): #this will later take as input the date n stuff. db as is now is 
         print("| Available Rooms by Number:         |")
         z = cursor.fetchall()
         n=10
-        if len(z) < n:
-            n = len(z)
+        l=0
+        loop = True
         print("+-----------+")
-        for i in range(n):
-            x = [z[i][0],z[i][2]]
-            print("Room Number: "+str(x[0]))
-            print("Price: $"+str(x[1]))
-            print("+-----------+")
-        print("For more information on each room, enter its room number. to see more rooms, press Y. To exit, press X")
-        inp = input("[ ]: ")
+        while loop:
+            if len(z) < n+l:
+                n = len(z)
+            for i in range(l,n+l):
+                x = [z[i][0],z[i][2]]
+                print("Room Number: "+str(x[0]))
+                print("Price: $"+str(x[1]))
+                print("+-----------+")
+            print("For more information on a room, enter its room number. to see more rooms, press E. To go back, press Q")
+            inp = input("[ ]: ")
+            if inp == "E":
+                l += 10
+            elif inp == "Q":
+                l -= 10
+            else:
+                room = int(inp)
+                cursor.execute("SELECT * FROM \"Project\".room WHERE \"Project\".room.room_num = \'"+inp+"\'")
+                g = cursor.fetchall()
+                print("Room Number: "+str(g[0][0]))
+                print("Is Booked: "+str(g[0][1]))
+                print("Price: $"+str(g[0][2]))
+                print("Has TV: "+str(g[0][3]))
+                print("Has AC: "+str(g[0][4]))
+                print("Room Capacity: "+str(g[0][5]))
+                print("View Rating: "+str(g[0][6]))
+                print("Can Extend: "+str(g[0][7]))
+                customer()
 
 def cBook():
     n = input("Enter Room Number: \n")
-    br = input("[1]: Booking\n[2]: Renting\nBooking or Renting?:\n")
-    if br == "1":
-        br = "booking"
-    else:
-        br = "renting"
+    br = "booking"
     chk = input("When will you check in?: (YYYY-MM-DD)\n")
     dur = input("How long will you stay?: \n")
     occ = input("How many occupants?: \n")
@@ -83,8 +89,7 @@ def cBook():
         if int(occ) > b:
             print("Too many people!")
         else:
-            print("asd")
-            cursor.execute("INSERT INTO \"Project\"."+br+" (room_num,check_in_date,stay_duration_days,num_ppl,occupant) VALUES ("+n+",\'"+chk+"\',"+dur+","+occ+",\'"+occ+"\')")
+            cursor.execute("INSERT INTO \"Project\"."+br+" (room_num,check_in_date,stay_duration_days,num_ppl,occupant) VALUES ("+n+",\'"+chk+"\',"+dur+","+occ+",\'"+nam+"\')")
             cursor.execute("UPDATE \"Project\".room SET is_booked = true WHERE \"Project\".room.room_num = \'"+n+"\'")
             con.commit()
     else:
@@ -99,98 +104,63 @@ def customer():
     print("+--------------------------+")
     x = input("[ ]:")
     if x == "1":
-
-        cSearch() #a and b take as args 
+        cSearch() 
     elif x == "2":
         cBook()
 
+def toRenting():
+    n = input("Enter Room Number:\n[ ]: " )
+    j = input("Credit Card Number:\n[ ]: ")
+    cursor.execute("SELECT * FROM \"Project\".booking WHERE \"Project\".booking.room_num = \'"+n+"\'")
+    data = cursor.fetchall()[0]
+    chk = data[1]
+    dur = data[2]
+    occ = data[3]
+    nam = data[4]
+    cursor.execute("INSERT INTO \"Project\".renting (room_num,check_in_date,stay_duration_days,num_ppl,occupant) VALUES ("+str(n)+",\'"+str(chk)+"\',"+str(dur)+","+str(occ)+",\'"+nam+"\') ")
+    #con.commit()
+    employee()
+
+def newRent():
+    n = input("Enter Room Number:\n[ ]: " )
+    j = input("Credit Card Number:\n[ ]: ")
+    cursor.execute("SELECT * FROM \"Project\".room WHERE \"Project\".room.room_num = \'"+n+"\'")
+    data = cursor.fetchall()[0]
+    chk = data[1]
+    dur = data[2]
+    occ = data[3]
+    nam = data[4]
+    cursor.execute("INSERT INTO \"Project\".renting (room_num,check_in_date,stay_duration_days,num_ppl,occupant) VALUES ("+str(n)+",\'"+str(chk)+"\',"+str(dur)+","+str(occ)+",\'"+nam+"\') ")
+    #con.commit()
+    employee()
+
+
 def employee():
-        print("Avaliable Rooms: ")
-        cursor.execute("SELECT room_num FROM \"Project\".Room")
-        print(cursor.fetchall())
-
-def admin():
-    while True:
-        cursor.execute(input()) #try-catch statement here
-        print(cursor.fetchall())
-
-
-
-
-title()
-# close connection
-con.close()
-    port="15432"
-)
-
-cursor = con.cursor()
-
-def printQuery(z, n=10):
-    if len(z) < n:
-        n = len(z)
-    print("+-----------+")
-    for i in range(n):
-        x = z[i]
-        for y in x: 
-            print("+ "+str(y))
-        print("+-----------+")
-    
-
-
-
-def title(): #main menu. admin and employee should prolly get prompted for passwords n stuff
-    title= "+------------------------------------+\n|                                    |\n|                                    |\n|                                    |\n|                                    |\n| [A]: Customer                      |\n| [B]: Employee                      |\n| [C]: Admin                         |\n|                                    |\n|                                    |\n+------------------------------------+"
-    print(title)
-    x = input("[ ]:")
-    if x == "A":
-        customer()
-    elif x == "B":
+    print("[1]: View Rooms Available")
+    print("[2]: View Bookings")
+    print("[3]: Booking to Renting")
+    print("[4]: New Renting")
+    z = input("[ ]: ")
+    if z == "3":
+        toRenting()
+    elif z == "1":
+        print("Avaliable Rooms by room num: ")
+        cursor.execute("SELECT room_num FROM \"Project\".room WHERE \"Project\".room.is_booked = FALSE")
+        for n in cursor.fetchall():
+            print(n[0])
         employee()
-    elif x == "C":
-        admin()
-
-def cSearch(): #this will later take as input the date n stuff. db as is now is empty so idk what to do with them
-        print("[1]: Daisu")
-        print("[2]: Ivay")
-        print("[3]: Shulizany")
-        print("[4]: Lame Places")
-        print("[5]: Lotsa Rooms")
-        print("[6]: Old Zealand")
-        print("[7]: Any")
-        print("Which Hotel Company?:")
-        x1 = input("[ ]:")
-        if x1 == "7":
-            x1 = "*"
-        x2 = input("Before/Exactly/After (B/E/A): \n")
-        x3 = input("Date: (YY-MM-DD)\n")
-        cursor.execute("SELECT * FROM \"Project\".hotel") #join room to 
-        print("| Avaliable Rooms by Number:         |")
-        printQuery(cursor.fetchall())
-
-def cBook():
-    n = input("Enter Room Number: \n")
-    br = input("[1]: Booking\n[2]: Renting\nBooking or Renting?:\n")
-    chk = input("When will you check in?: (YY-MM-DD)\n")
-    occ = input("How many occupants?: \n")
-
-    # *do some stuff*
-
-def customer():
-    print("+--------------------------+")
-    print("1: Search Avaliable Rooms")
-    print("2: Book a Room")
-    print("+--------------------------+")
-    x = input("[ ]:")
-    if x == "1":
-
-        cSearch() #a and b take as args 
-    elif x == "2":
-        cBook()
-
-def employee():
-        print("Avaliable Rooms: ")
-        cursor.execute("SELECT room_num FROM \"Project\".Room")
-        print(cursor.fetchall())
+    elif z == "2":
+        print("Current Rooms booked by room num: ")
+        cursor.execute("SELECT room_num FROM \"Project\".booking")
+        for n in cursor.fetchall():
+            print(n[0])
+        employee()
+    elif z == "4":
+        
+        #print(cursor.fetchall())
+    
+    
+    #print(cursor.fetchall())
 
 def admin():
     while True:
